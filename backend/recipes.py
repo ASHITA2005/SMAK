33a05458738017f35b6fb5e558ac92898e541235
FayizS <fayizsameer@gmail.com>
2025-10-08 15:52:48 +0530

    removed bench

--- backend/recipes.py
+++ backend/recipes.py
@@ -1,18 +1,18 @@
 recipes={
     "burger":{
         "tasks":{
-            "t1":{"name":"form_patties", "duration":2, "resource":"bench"},
+            "t1":{"name":"form_patties", "duration":2, "resource":"countertop"},
             "t2":{"name":"grill_patties", "duration":6, "resource":"grill"},
             "t3":{"name":"toast_buns", "duration":1, "resource":"toaster"},
-            "t4":{"name":"assemple", "duration":1, "resource":"bench"}
+            "t4":{"name":"assemple", "duration":1, "resource":"countertop"}
         },
         "deps":[("t1", "t2"), ("t3", "t4"), ("t2", "t4")]
     },
     "fries": {
         "tasks": {
-            "f1": {"name": "cut_potatoes", "duration": 2, "resource": "bench"},
+            "f1": {"name": "cut_potatoes", "duration": 2, "resource": "countertop"},
             "f2": {"name": "fry_potatoes", "duration": 4, "resource": "fryer"},
-            "f3": {"name": "salt_fries", "duration": 1, "resource": "bench"},
+            "f3": {"name": "salt_fries", "duration": 1, "resource": "countertop"},
         },
         "deps": [("f1", "f2"), ("f2", "f3")]
     }
