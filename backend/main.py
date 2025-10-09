2f7417fc4a4b9bb725a05d373fbd8b805d1d3dbd
FayizS <fayizsameer@gmail.com>
2025-10-09 19:01:49 +0530

    Started on scheduling function

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,23 @@
+from collections import deque
 from recipes import recipes
+from threading import Semaphore
 
 for name in recipes:
     print(name)
+
+#Resources Initialization
+countertop = Semaphore(5)
+grill = Semaphore(1)
+stove = Semaphore(3)
+toaster = Semaphore(1)
+fryer = Semaphore(2)
+
+
+while ready or finish_ready:
+    sorted_ready = sorted(ready, )
+    new_ready = deque()
+    scheduled = False
+
+    for t in sorted_ready:
+        if t["resource"]
 
